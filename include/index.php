3bebbc7afbbe0662a01e505da229655bf9276cbd
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-16 13:12:34 +0100

    m

---
+++ include/index.php
@@ -0,0 +1,11 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> SvxBuilder</title>
+    </head>
+    <body>
+        <h1>Building an SvxLink Node</h1>
+        <p>Ready... set... go!</p>
+    </body>
+</html>

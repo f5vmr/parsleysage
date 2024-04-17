408d202ff817ed874329ab66f5b5b3ef6d6456a0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-17 12:59:58 +0100

    m

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install php8.2 apache2 lib --fix-missing
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install php8.2 apache2 --fix-missing
 clear
 echo Now go to your browser to begin. type in the IP address as the URL

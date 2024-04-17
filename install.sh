2431f605def5a828bd4541c79289b190291cf420
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-17 13:02:29 +0100

    m

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install php8.2 apache2 --fix-missing
 clear
 echo Now go to your browser to begin. type in the IP address as the URL
+cd parsleysage
+sudo mv * /html
+

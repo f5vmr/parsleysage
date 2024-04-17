18b7ad4322b7a617b254e4cf3a3d90ed18e0b91c
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-17 18:18:56 +0100

    m

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install php8.2 apache2 --fix-missing
+
 cd /home/pi
 sudo git clone https://github.com/f5vmr/svxlinkbuilder
 
@@ -8,6 +9,8 @@ clear
 echo Now go to your browser to begin. type in the IP address as the URL
 cd parsleysage
 sudo mv * /var/www/html
+cd /home/pi
+sudo git clone http://github.com/sm0svx/svxlink
 cd svxlink
 
 # ./svxlinkbuilder/preinstall.sh

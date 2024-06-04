d85df8e9f7d954c171a1b346e1251cefe8c5e7bb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-04 16:40:11 +0100

    m

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##### Configuration file a new build script
 sudo apt update && sudo apt upgrade -y
-sudo apy install -y  libgpiod2 libgpiod-dev gpiod 
+sudo apt install -y  libgpiod2 libgpiod-dev gpiod 
 sudo echo "deb [ trusted=yes ] deb https://deb.debian.org/debian trixie main"/etc/apt/sources.list.d/trixie.list
 sudo apt update
 sudo apt install svxlink-server -y

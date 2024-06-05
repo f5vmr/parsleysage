208deddad8c0272d28ac7e5a2f086dbe87e20bb8
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-05 14:42:57 +0100

    m

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y  libgpiod2 libgpiod-dev gpiod 
 sudo wget https://debian.mirror.beocat.ksu.edu/archive/pool/main/s/svxlink/svxlink-server_24.02-1_armhf.deb
 
-sudo dpkg -i svxlink-server_24.02-1_armhf.deb --install-recommends --force-depends  
+sudo dpkg -i svxlink-server_24.02-1_armhf.deb   
 sudo apt install -f -y
 
